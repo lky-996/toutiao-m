3c260bdefa751db0176f8054737691661090be02
lky996 <983544200@qq.com>
2021-03-07 13:10:46 +0800

    引入vant组件库

--- src/main.js
+++ src/main.js
@@ -3,8 +3,17 @@ import App from './App.vue'
 import router from './router'
 import store from './store'
 
+// 引入vant组件库
+import Vant from 'vant'
+
+// 引入vant组件库样式
+import 'vant/lib/index.css'
+
 // 导入全局样式
 import './styles/index.less'
+
+// 注册vant组件
+Vue.use(Vant)
 
 Vue.config.productionTip = false
 
